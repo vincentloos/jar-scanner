5187d80df640cc643dd734a3ae838a688b4faf51
VincentLoos <vincent@vlosoft.com>
2019-11-30 22:54:18 +0100

    Add main scene

--- src/main/java/dev/vloos/javahccs/Main.java
+++ src/main/java/dev/vloos/javahccs/Main.java
@@ -1,7 +1,28 @@
 package dev.vloos.javahccs;
 
-public class Main {
-    public static void main(String args[]) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+
+        Scene scene = new Scene(root, 425, 220);
+
+        stage.setResizable(false);
+        stage.setTitle("Java HCCS");
+        stage.setScene(scene);
+        stage.show();
     }
 }
